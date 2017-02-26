19f68c1624cc036811c6ba3bf647f50ca8e6a964
Sunny NG <ngsanluk@gmail.com>
2017-02-26 19:08:39 +0800

    Initial commit!

--- ForwardGeocodingDemo/ViewController.swift
+++ ForwardGeocodingDemo/ViewController.swift
@@ -7,9 +7,38 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var addressTextField: UITextField!
+    
+    @IBOutlet weak var labelLatLng: UILabel!
+    
+
+    @IBAction func doForwardGeocoding(_ sender: Any) {
+        let address = addressTextField.text!
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(address, completionHandler: {(placemarks, error) in
+            if error != nil {
+                self.labelLatLng.text = "Error: " + error!.localizedDescription
+            }
+            else if placemarks!.count == 0 {
+                self.labelLatLng.text = "Can't found a match"
+            }
+            else if placemarks!.count > 0 {
+                let placemark = placemarks![0]
+                let location = placemark.location
+                let coords = location!.coordinate
+                self.labelLatLng.text = "lat:\(coords.latitude), lng:\(coords.longitude)"
+                
+                
+            }
+            
+        })
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
---
+++ README.md
@@ -0,0 +1,2 @@
+# iOS Forward Geocoding Demo
+This example shows translating an human readable address string to CoreLocation CLPlacemark that encauplate latitude, longitude and other related geographical information
